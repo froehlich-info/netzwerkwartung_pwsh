41c70f32271ca092d4fd78448b3c291ee38612e9
Frederic Dreher @ Fröhlich Info AG <technik@froehlich.ch>
2023-01-07 18:58:23 +0100

    Create main.ps1
    local info gathering about a system , generated by ChatGPT
    
    Starting to test

---
+++ main.ps1
@@ -0,0 +1,34 @@
+# Gather information about a local machine, a starting point
+
+# Get the current date and time
+$date = Get-Date
+
+# Get the operating system information
+$os = Get-WmiObject -Class Win32_OperatingSystem
+
+# Get the installed software
+$software = Get-WmiObject -Class Win32_Product | Select-Object -Property Name, Version
+
+# Get the system performance
+$cpu = Get-WmiObject -Class Win32_Processor | Select-Object -Property Name, NumberOfCores, NumberOfLogicalProcessors
+$memory = Get-WmiObject -Class Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum
+$disk = Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DriveType -eq 3} | Select-Object -Property DeviceID, FreeSpace, Size
+
+# Output the results
+"OS Information"
+"-------------"
+"Operating System: $($os.Caption)"
+"Version: $($os.Version)"
+"Manufacturer: $($os.Manufacturer)"
+"Build Number: $($os.BuildNumber)"
+
+"Installed Software"
+"------------------"
+$software
+
+"System Performance"
+"------------------"
+"CPU: $($cpu.Name) with $($cpu.NumberOfCores) cores and $($cpu.NumberOfLogicalProcessors) logical processors"
+"Memory: $([Math]::Round(($memory.Sum / 1GB), 2)) GB"
+"Disk Space: "
+$disk | Format-Table -AutoSize
